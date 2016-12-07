36349da537b4a9b8156ff75cf955f3b461e8a65d
Fabrizio Lungo <git@flungo.me>
2016-12-07 16:03:12 +0000

    Initial commit

---
+++ src/main/java/uk/ac/soton/ecs/comp3204/scenerecog/App.java
@@ -0,0 +1,15 @@
+package uk.ac.soton.ecs.comp3204.scenerecog;
+
+/**
+ *
+ */
+public class App {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO: code application logic here
+    }
+    
+}
