47e56077f09a1eae01480241734f2399fa9c4ccc
Fabrizio Lungo <git@flungo.me>
2016-12-13 14:28:41 +0000

    Use testing folder inside testing.zip

--- src/main/java/uk/ac/soton/ecs/comp3204/scenerecog/DatasetUtil.java
+++ src/main/java/uk/ac/soton/ecs/comp3204/scenerecog/DatasetUtil.java
@@ -11,31 +11,31 @@ import org.openimaj.data.dataset.VFSListDataset;
 import org.openimaj.image.ImageUtilities;
 
 /**
- * Utility for loading the testing and training datasets (as zips) from a given directory. The zips should be
- * named {@code training.zip} and {@code testing.zip}.
+ * Utility for loading the testing and training datasets (as zips) from a given directory. The zips should be named
+ * {@code training.zip} and {@code testing.zip}.
  */
 public final class DatasetUtil {
-    
+
     private static final Logger LOGGER = Logger.getLogger(DatasetUtil.class.getName());
-    
+
     public static final String REMOTE_DATASET_DIRECTORY = "http://comp3204.ecs.soton.ac.uk/cw/";
     public static final String TRAINING_DATASET = "training.zip!/training";
-    public static final String TESTING_DATASET = "testing.zip";
-    
+    public static final String TESTING_DATASET = "testing.zip!/testing";
+
     private URI directory;
 
     public DatasetUtil(URI directory) {
         this.directory = directory;
     }
-    
+
     public VFSGroupDataset getTraining() throws FileSystemException {
         return new VFSGroupDataset("zip:" + directory.resolve(TRAINING_DATASET).toString(), ImageUtilities.FIMAGE_READER);
     }
-    
+
     public VFSListDataset getTesting() throws FileSystemException {
         return new VFSListDataset("zip:" + directory.resolve(TESTING_DATASET).toString(), ImageUtilities.FIMAGE_READER);
     }
-    
+
     public static DatasetUtil getDatasetUtil() throws URISyntaxException {
         File localFile = new File("data").getAbsoluteFile();
         if (localFile.isDirectory()) {
@@ -46,5 +46,5 @@ public final class DatasetUtil {
             return new DatasetUtil(new URI(REMOTE_DATASET_DIRECTORY));
         }
     }
-    
+
 }
