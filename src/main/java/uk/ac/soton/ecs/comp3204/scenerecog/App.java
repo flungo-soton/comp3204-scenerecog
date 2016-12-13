ad7209e0857b9343193e975750921d15cad8ff5f
Fabrizio Lungo <git@flungo.me>
2016-12-13 22:10:27 +0000

    Implemented evaluation.
    Classification now evaluates the classifier when run. It no longer takes constructor arguments and takes the arguments when being run.
    
    App now handles all console printing with a run method.

--- src/main/java/uk/ac/soton/ecs/comp3204/scenerecog/App.java
+++ src/main/java/uk/ac/soton/ecs/comp3204/scenerecog/App.java
@@ -1,29 +1,42 @@
 package uk.ac.soton.ecs.comp3204.scenerecog;
 
+import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.apache.commons.vfs2.FileSystemException;
+import org.openimaj.experiment.evaluation.classification.analysers.confusionmatrix.CMResult;
 
 /**
  * Runner for the Scene Recognition coursework. This will run all 3 recognisers.
  */
 public class App {
-    
+
     private static final Logger LOGGER = Logger.getLogger(App.class.getName());
+
+    public static final double TRAINING_PCT = 0.85;
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        DatasetUtil datasets;
         try {
-            DatasetUtil datasets = DatasetUtil.getDatasetUtil();
-            
-            System.out.println(datasets.getTraining());
-            System.out.println(datasets.getTesting());
+            datasets = DatasetUtil.getDatasetUtil();
+
+            LOGGER.log(Level.CONFIG, "Training dataset: {0}", datasets.getTraining());
+            LOGGER.log(Level.CONFIG, "Testing dataset: {0}", datasets.getTesting());
         } catch (FileSystemException | URISyntaxException ex) {
             LOGGER.log(Level.SEVERE, "Exception loading datasets.", ex);
+            return;
         }
     }
-    
+
+    public static void run(Classification classification, String runID, DatasetUtil datasets) throws IOException {
+        System.out.println("############ Run " + runID + " ############");
+        System.out.println();
+        CMResult<String> performance = classification.run(datasets, "run" + runID + ".txt", TRAINING_PCT);
+        System.out.println(performance.getDetailReport());
+        System.out.println();
+    }
 }
--- src/main/java/uk/ac/soton/ecs/comp3204/scenerecog/Classification.java
+++ src/main/java/uk/ac/soton/ecs/comp3204/scenerecog/Classification.java
@@ -1,6 +1,10 @@
 package uk.ac.soton.ecs.comp3204.scenerecog;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.apache.commons.vfs2.FileSystemException;
@@ -9,29 +13,24 @@ import org.openimaj.data.dataset.GroupedDataset;
 import org.openimaj.data.dataset.ListDataset;
 import org.openimaj.data.dataset.ReadableListDataset;
 import org.openimaj.data.identity.IdentifiableObject;
+import org.openimaj.experiment.evaluation.classification.ClassificationEvaluator;
+import org.openimaj.experiment.evaluation.classification.ClassificationResult;
+import org.openimaj.experiment.evaluation.classification.analysers.confusionmatrix.CMAnalyser;
+import org.openimaj.experiment.evaluation.classification.analysers.confusionmatrix.CMResult;
+import org.openimaj.experiment.validation.StratifiedGroupedRandomisedPercentageHoldOut;
 import org.openimaj.image.FImage;
 import org.openimaj.ml.annotation.Annotator;
-import org.openimaj.ml.annotation.IncrementalAnnotator;
 
 /**
  * Classify the dataset given and write the result in given file.
  *
  * @param <A> The type of annotator that is created.
  */
-public abstract class Classification<A extends Annotator> implements Runnable {
+public abstract class Classification<A extends Annotator> {
 
     private static final Logger LOGGER = Logger.getLogger(Classification.class.getName());
 
-    private final DatasetUtil dsUtil;
-    private final String output;
-
-    public Classification(DatasetUtil dsUtil, String output) {
-        this.dsUtil = dsUtil;
-        this.output = output;
-    }
-
-    @Override
-    public void run() {
+    public CMResult<String> run(DatasetUtil dsUtil, String output, double trainingPercent) throws IOException {
         LOGGER.log(Level.INFO, "Running classifier.");
 
         GroupedDataset<String, ? extends ListDataset<FImage>, FImage> training;
@@ -41,10 +40,13 @@ public abstract class Classification<A extends Annotator> implements Runnable {
             training = dsUtil.getTraining();
             testing = dsUtil.getTesting();
         } catch (FileSystemException ex) {
-            LOGGER.log(Level.SEVERE, "Error loading datasets", ex);
-            return;
+            throw new IOException("Error loading datasets", ex);
         }
 
+        // Evaluate the performance
+        CMResult<String> performance = evaluate(training, trainingPercent);
+
+        // Train and classify the test data
         GroupedDataset<String, ? extends Dataset<IdentifiableObject<FImage>>, IdentifiableObject<FImage>> classifications = trainAndClassify(training, testing);
 
         try {
@@ -52,8 +54,10 @@ public abstract class Classification<A extends Annotator> implements Runnable {
             GroupWriter<String, IdentifiableObject<FImage>> writer = new IdentifyiableGroupWriter(output);
             writer.write(classifications);
         } catch (IOException ex) {
-            LOGGER.log(Level.SEVERE, "IOException writing classifications to file", ex);
+            throw new IOException("IOException writing classifications to file", ex);
         }
+
+        return performance;
     }
 
     public abstract AnnotatorWrapper<A> getAnnotatorWrapper();
@@ -70,5 +74,37 @@ public abstract class Classification<A extends Annotator> implements Runnable {
         LOGGER.log(Level.FINE, "Classifying the test data");
         DatasetClassifier<String, FImage> classifier = new DatasetClassifier(annotatorWrapper.getAnnotator());
         return classifier.classifyIdentifiable(testing.toIdentifiable());
+    }
+
+    public CMResult<String> evaluate(GroupedDataset<String, ? extends ListDataset<FImage>, FImage> training, double trainingPercent) {
+        // Get the annotator
+        AnnotatorWrapper<A> annotator = getAnnotatorWrapper();
+
+        // Split the training data
+        StratifiedGroupedRandomisedPercentageHoldOut<String, FImage> splits = new StratifiedGroupedRandomisedPercentageHoldOut(trainingPercent, training);
+
+        // Train with the training data
+        annotator.train(splits.getTrainingDataset());
+
+        // Build map of validation images
+        // Passing splits.getValidationDataset() results in cloned images and does not work.
+        // TODO: Investigate why
+        GroupedDataset<String, ListDataset<FImage>, FImage> validationDataset = splits.getValidationDataset();
+        Map<FImage, Set<String>> validationMap = new HashMap<>();
+        for (String group : training.getGroups()) {
+            for (FImage image : validationDataset.getInstances(group)) {
+                Set<String> classes = new HashSet<>();
+                classes.add(group);
+                validationMap.put(image, classes);
+            }
+        }
+
+        // Validate the data
+        ClassificationEvaluator<CMResult<String>, String, FImage> eval
+                = new ClassificationEvaluator<CMResult<String>, String, FImage>(
+                        annotator.getAnnotator(), validationMap, new CMAnalyser<FImage, String>(CMAnalyser.Strategy.SINGLE));
+
+        Map<FImage, ClassificationResult<String>> guesses = eval.evaluate();
+        return eval.analyse(guesses);
     }
 }
