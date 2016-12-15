937bda7c6dfbc3308544ff7d81606cbf4dad0bd5
Fabrizio Lungo <git@flungo.me>
2016-12-15 12:22:20 +0000

    Moved classifcation run to the app.
    Allows report to be produced before classifcation is done.

--- src/main/java/uk/ac/soton/ecs/comp3204/scenerecog/App.java
+++ src/main/java/uk/ac/soton/ecs/comp3204/scenerecog/App.java
@@ -8,7 +8,13 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.apache.commons.io.output.TeeOutputStream;
 import org.apache.commons.vfs2.FileSystemException;
+import org.openimaj.data.dataset.Dataset;
+import org.openimaj.data.dataset.GroupedDataset;
+import org.openimaj.data.dataset.ListDataset;
+import org.openimaj.data.dataset.ReadableListDataset;
+import org.openimaj.data.identity.IdentifiableObject;
 import org.openimaj.experiment.evaluation.classification.analysers.confusionmatrix.CMResult;
+import org.openimaj.image.FImage;
 import uk.ac.soton.ecs.comp3204.scenerecog.run1.Run1;
 import uk.ac.soton.ecs.comp3204.scenerecog.run2.Run2;
 import uk.ac.soton.ecs.comp3204.scenerecog.run3.Run3;
@@ -21,6 +27,11 @@ public class App {
     private static final Logger LOGGER = Logger.getLogger(App.class.getName());
 
     public static final double TRAINING_PCT = 0.85;
+
+    private static final int BOVW_PATCH_SIZE = 8;
+    private static final int BOVW_PATCH_STEP = 4;
+    private static final int BOVW_PATCHES_PER_IMAGE = 10;
+    private static final int BOVW_K_MEANS = 500;
 
     /**
      * @param args the command line arguments
@@ -49,31 +60,76 @@ public class App {
         }
         try {
             // Run, Run2
-            run(new Run2(), "2", datasets);
+            run(new Run2(BOVW_PATCH_SIZE, BOVW_PATCH_STEP, BOVW_PATCHES_PER_IMAGE, BOVW_K_MEANS), "2", datasets);
         } catch (IOException e) {
-            LOGGER.log(Level.SEVERE, "IOException in Run 2.", e);
-            return;
+            LOGGER.log(Level.SEVERE, "IOException running classification run 2.", e);
         }
         try {
             // Run, Run3
             run(new Run3(datasets), "3", datasets);
         } catch (IOException e) {
-            LOGGER.log(Level.SEVERE, "IOException in Run 3.", e);
-            return;
+            LOGGER.log(Level.SEVERE, "IOException running classification run 3.", e);
         }
     }
 
+    /**
+     * Run a classification to create an evaluation report and the
+     * classification of the test data.
+     *
+     * @param classification the classification to run.
+     * @param runID the unique ID of the run, to identify the output files.
+     * @param datasets the dataset utility which provides the training and
+     * testing data.
+     * @throws IOException if there is an error reading the datasets or writing
+     * the reports.
+     */
     public static void run(Classification classification, String runID, DatasetUtil datasets) throws IOException {
+        LOGGER.log(Level.INFO, "Running classifier: {0}", runID);
+
+        // Files
+        String evaluationOutput = "run" + runID + "_report.txt";
+        String classificationOutput = "run" + runID + ".txt";
+
+        // Setup a split output to console and file.
         PrintStream out = new PrintStream(
                 new TeeOutputStream(
                         System.out,
-                        new FileOutputStream("run" + runID + "_report.txt")
+                        new FileOutputStream(evaluationOutput)
                 )
         );
+
+        // Load the training and testing data.
+        GroupedDataset<String, ? extends ListDataset<FImage>, FImage> training;
+        ReadableListDataset<FImage, ?> testing;
+        try {
+            LOGGER.log(Level.FINE, "Loading datasets");
+            training = datasets.getTraining();
+            testing = datasets.getTesting();
+        } catch (FileSystemException ex) {
+            throw new IOException("Error loading datasets", ex);
+        }
+
         out.println("############ Run " + runID + " ############");
         out.println();
-        CMResult<String> performance = classification.run(datasets, "run" + runID + ".txt", TRAINING_PCT);
+
+        // Evaluate the performance
+        LOGGER.log(Level.FINE, "Evaluating the performance of the classifier");
+        CMResult<String> performance = classification.evaluate(training, TRAINING_PCT);
         out.println(performance.getDetailReport());
         out.println();
+
+        // Do the classification
+        // Train and classify the test data
+        LOGGER.log(Level.FINE, "Training and classifying testing data");
+        GroupedDataset<String, ? extends Dataset<IdentifiableObject<FImage>>, IdentifiableObject<FImage>> classifications
+                = classification.trainAndClassify(training, testing);
+
+        try {
+            LOGGER.log(Level.FINE, "Writing classifications to {0}", classificationOutput);
+            GroupWriter<String, IdentifiableObject<FImage>> writer = new IdentifyiableGroupWriter(classificationOutput);
+            writer.write(classifications);
+        } catch (IOException ex) {
+            throw new IOException("IOException writing classifications to file", ex);
+        }
     }
 }
--- src/main/java/uk/ac/soton/ecs/comp3204/scenerecog/Classification.java
+++ src/main/java/uk/ac/soton/ecs/comp3204/scenerecog/Classification.java
@@ -1,13 +1,11 @@
 package uk.ac.soton.ecs.comp3204.scenerecog;
 
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import org.apache.commons.vfs2.FileSystemException;
 import org.openimaj.data.dataset.Dataset;
 import org.openimaj.data.dataset.GroupedDataset;
 import org.openimaj.data.dataset.ListDataset;
@@ -29,36 +27,6 @@ import org.openimaj.ml.annotation.Annotator;
 public abstract class Classification<A extends Annotator> {
 
     private static final Logger LOGGER = Logger.getLogger(Classification.class.getName());
-
-    public CMResult<String> run(DatasetUtil dsUtil, String output, double trainingPercent) throws IOException {
-        LOGGER.log(Level.INFO, "Running classifier.");
-
-        GroupedDataset<String, ? extends ListDataset<FImage>, FImage> training;
-        ReadableListDataset<FImage, ?> testing;
-        try {
-            LOGGER.log(Level.FINE, "Loading datasets");
-            training = dsUtil.getTraining();
-            testing = dsUtil.getTesting();
-        } catch (FileSystemException ex) {
-            throw new IOException("Error loading datasets", ex);
-        }
-
-        // Evaluate the performance
-        CMResult<String> performance = evaluate(training, trainingPercent);
-
-        // Train and classify the test data
-        GroupedDataset<String, ? extends Dataset<IdentifiableObject<FImage>>, IdentifiableObject<FImage>> classifications = trainAndClassify(training, testing);
-
-        try {
-            LOGGER.log(Level.FINE, "Writing classifications to {0}", output);
-            GroupWriter<String, IdentifiableObject<FImage>> writer = new IdentifyiableGroupWriter(output);
-            writer.write(classifications);
-        } catch (IOException ex) {
-            throw new IOException("IOException writing classifications to file", ex);
-        }
-
-        return performance;
-    }
 
     public abstract AnnotatorWrapper<A> getAnnotatorWrapper();
 
